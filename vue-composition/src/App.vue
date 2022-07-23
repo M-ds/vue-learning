6c89a2b9a415d193eaeb6d326b1937b5e068097d
Michiel de Smet <michiel.desmet@dxc.com>
2022-07-23 11:48:42 +0200

    vue-composition - setup

---
+++ vue-composition/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from "vue";
+
+// Reactive references.
+const header = ref("Shopping List App");
+const items = ref([
+  { id: 1, label: "10 party hats" },
+  { id: 2, label: "2 board games" },
+  { id: 3, label: "20 cups" },
+]);
+</script>
+
+<template>
+  <h1>{{ header }}</h1>
+  <ul>
+    <li v-for="{ id, label } in items" :key="id">{{ label }}</li>
+  </ul>
+</template>
+
+<style lang="scss">
+//
+</style>
