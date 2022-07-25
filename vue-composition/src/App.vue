3218683022f656cbcdded3fb34fd0626b0898aff
Michiel de Smet <michiel.desmet@dxc.com>
2022-07-25 08:25:01 +0200

    vue-composition - added some more functions

--- vue-composition/src/App.vue
+++ vue-composition/src/App.vue
@@ -1,17 +1,30 @@
 <script setup>
 import { ref } from "vue";
 
-// Reactive references.
+// ref =  Reactive references.
 const header = ref("Shopping List App");
 const items = ref([
   { id: 1, label: "10 party hats" },
   { id: 2, label: "2 board games" },
   { id: 3, label: "20 cups" },
 ]);
+const newItem = ref("");
+const newItemHighPriority = ref(false);
 </script>
 
 <template>
   <h1>{{ header }}</h1>
+  <form
+    class="add-item-form"
+    @submit.prevent="items.push({ id: items.length + 1, label: newItem })"
+  >
+    <input v-model.trim="newItem" type="text" placeholder="Add an item" />
+    <label>
+      <input type="checkbox" v-model="newItemHighPriority" />
+      High Priority
+    </label>
+    <button class="btn btn-primary">Save item</button>
+  </form>
   <ul>
     <li v-for="{ id, label } in items" :key="id">{{ label }}</li>
   </ul>
