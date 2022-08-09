5e135c53610af881da284df359db352d853af446
Michiel de Smet <michiel.desmet@dxc.com>
2022-08-09 09:13:34 +0200

    vue-custom-directive - WIP

---
+++ custom-directive/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from "vue";
+// ref =  Reactive references.
+const colours = ref(["purple", "gold", "pink", "blue", "red"]);
+const show = ref(false);
+</script>
+
+<template>
+  <button @click="show = !show">toggle</button>
+  <div v-if="show">
+    <h1 v-purple>Purple Cat Software</h1>
+    <h1 v-switching-color:normal.underline.textcenter="colours">
+      Purple Cat Software
+    </h1>
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ custom-directive/src/main.ts
@@ -0,0 +1,54 @@
+import { createApp, DirectiveBinding } from "vue";
+import App from "./App.vue";
+
+const app = createApp(App);
+
+app.directive("purple", {
+  beforeMount(el: HTMLElement, binding: DirectiveBinding) {
+    el.style.color = "purple";
+  },
+});
+
+app.directive("switching-color", {
+  beforeMount(el: HTMLElement, binding: DirectiveBinding) {
+    const modifiers = binding.modifiers;
+    if (modifiers.underline) {
+      el.style.textDecoration = "underline";
+    }
+    if (modifiers.textcenter) {
+      el.style.textAlign = "center";
+    }
+
+    const speeds = {
+      slow: 2000,
+      normal: 1000,
+      fast: 500,
+      crazy: 100,
+    };
+    const speedName: string = binding.arg || "normal";
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const speed = speeds[speedName];
+    const colors = binding.value;
+    let i = 0;
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    el.interval = setInterval(() => {
+      el.style.color = colors[i++];
+      if (i > colors.length - 1) {
+        i = 0;
+      }
+    }, speed);
+  },
+  /*
+  Het is niet mogelijk om elementen door te sturen in verschillende hooks zoals beforeMount en beforeUnmount.
+  Daarom zetten we een functie op het element.
+   */
+  beforeUnmount(el: HTMLElement) {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    clearInterval(el.interval);
+  },
+});
+
+app.mount("#app");
