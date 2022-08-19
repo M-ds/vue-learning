872d03e852b4e6855ade1c81e0bcfd0734a9cb5e
Michiel de Smet <michiel.desmet@dxc.com>
2022-08-19 15:15:37 +0200

    vue-slot - WIP

---
+++ slots/src/components/LayoutComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="layout">
+    <header>
+      <slot name="header" />
+    </header>
+    <main>
+      <slot />
+    </main>
+    <footer>
+      <slot name="footer" />
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LayoutComponent",
+};
+</script>
+
+<style scoped>
+.layout {
+  border-top-color: green;
+  border-top: 100px;
+}
+</style>
